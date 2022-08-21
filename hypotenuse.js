190bdee8f908513e077a6477669e6dbadabc28da
Anuj27aKamboj <anujkamboj2000@gmail.com>
2022-08-21 16:31:35 +0530

    update: App ready

---
+++ hypotenuse.js
@@ -0,0 +1,19 @@
+const sideInputs = document.querySelectorAll(".side-input");
+const hypotenuseBtn = document.querySelector("#hypotenuse-btn");
+const outputEl = document.querySelector("#output");
+
+function calculateSumOfSquares(l1, l2){
+    const sumOfSquares = (l1**2) + (l2**2);
+    return sumOfSquares;
+}
+
+function calculateHypotenuse(){
+    const sumOfSquares = calculateSumOfSquares(Number(sideInputs[0].value), Number(sideInputs[1].value));
+    const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
+    outputEl.innerText = "Hypotenuse is : "+lengthOfHypotenuse;
+
+}
+
+
+
+hypotenuseBtn.addEventListener("click", calculateHypotenuse);
---
+++ isTriangle.js
@@ -0,0 +1,29 @@
+const inputs = document.querySelectorAll(".angle-input");
+const isTriangleBtn = document.querySelector("#is-trinagle-btn");
+const outputText = document.querySelector(".output");
+
+
+function calculateSumOfAngle(angle1, angle2, angle3){
+    const sumOfAngles = angle1 + angle2 + angle3;
+    return sumOfAngles;
+}
+
+
+
+
+
+function isTrinagle(){
+    const sumOfAngles = calculateSumOfAngle(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value))
+
+    if(sumOfAngles === 180){
+        outputText.innerText= "Yay! Angles form a triangle";
+    }else{
+        outputText.innerText= "Ohh! Angles don't form a triangle";
+    }
+}
+
+
+
+
+
+isTriangleBtn.addEventListener("click", isTrinagle);
